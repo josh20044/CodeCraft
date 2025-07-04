9e0ac4d871f9eb528cc1cfad3c491dc236d01b33
josh22 <126473171+josh20044@users.noreply.github.com>
2025-07-05 00:04:49 +0800

    fire update

--- login_menu_interface.gd
+++ login_menu_interface.gd
@@ -6,7 +6,10 @@ var icon_close = preload("res://Icons/eyehidden.png")
 var icon_open = preload("res://Icons/eye.png")
 
 func _ready() -> void:
-	pass
+	Firebase.Auth.login_succeeded.connect(on_login_succeeded)
+	Firebase.Auth.signup_succeeded.connect(on_signup_succeeded)
+	Firebase.Auth.login_failed.connect(on_login_failed)
+	Firebase.Auth.signup_failed.connect(on_signup_failed)
 
 func _process(delta: float) -> void:
 	if iCanSeeYourPassSir == true:
@@ -36,3 +39,38 @@ func _on_register_now_button_gui_input(event: InputEvent) -> void:
 func _on_forget_password_button_gui_input(event: InputEvent) -> void:
 	if event.as_text() == "Left Mouse Button":
 		UiSignals.open_forgot.emit()
+
+# firebase
+
+func on_login_succeeded(auth):
+	print("Login success!")
+	#$StateLabel.text = "Login success!"
+	Firebase.Auth.save_auth(auth)
+	get_tree().change_scene_to_file("res://Game.tscn")
+	
+func on_signup_succeeded(auth):
+	#print(auth)
+	#$StateLabel.text = "Sign up success!"
+	Firebase.Auth.save_auth(auth)
+	get_tree().change_scene_to_file("res://Game.tscn")
+	
+func on_login_failed(error_code, message):
+	print(error_code)
+	print(message)
+	#$StateLabel.text = "Login failed. Error: %s" % message
+	
+func on_signup_failed(error_code, message):
+	print(error_code)
+	print(message)
+	#$StateLabel.text = "Sign up failed. Error: %s" % message
+
+
+func _on_login_button_pressed() -> void:
+	var email = $Panel/VBoxContainer/EmailTextBox.text
+	var password = $Panel/VBoxContainer/PasswordTextBox.text
+	Firebase.Auth.login_with_email_and_password(email, password)
+
+
+func _on_google_sign_in_button_pressed() -> void:
+	var provider : AuthProvider = Firebase.Auth.get_GoogleProvider()
+	Firebase.Auth.get_auth_localhost(provider, 8060)
